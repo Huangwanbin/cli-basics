a7c080f8b49b2fbd3c030e0709fbe3eb2f48045e
huangwanbin <wanbin_huang@qingsongchou.com>
2021-06-29 09:34:15 +0800

    add chalk

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,26 @@
 #! /usr/bin/env node
 const program = require('commander');
+const chalk = require('chalk');
 
 program
     .version('0.0.1')
     .command('create <name>')
     .description('create a new project')
     .action(name => {
-        console.log('your project name :',name, 'right?')
+        // 打印命令行输入的值
+        // 文本样式
+        console.log("project name is " + chalk.bold(name))
+
+        // 颜色
+        console.log("project name is " + chalk.cyan(name))
+        console.log("project name is " + chalk.green(name))
+
+        // 背景色
+        console.log("project name is " + chalk.bgRed(name))
+
+        // 使用RGB颜色输出
+        console.log("project name is " + chalk.rgb(4, 156, 219).underline(name));
+        console.log("project name is " + chalk.hex('#049CDB').bold(name));
+        console.log("project name is " + chalk.bgHex('#049CDB').bold(name))
     });
 program.parse();
