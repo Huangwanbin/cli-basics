af1e2417953dd049b11ae47a3011f6a703e928b5
huangwanbin <wanbin_huang@qingsongchou.com>
2021-06-29 09:46:11 +0800

    add cross-spawn

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,11 @@
 #! /usr/bin/env node
-const program = require('commander');
-const chalk = require('chalk');
-const inquirer = require('inquirer');
-const ora = require('ora');
+const program = require('commander'); //自定义命令行指令
+const chalk = require('chalk'); //命令行美化工具
+const inquirer = require('inquirer'); //命令行交互工具
+const ora = require('ora'); //loading动效
+const crossSpawn = require('cross-spawn'); //跨平台shell执行工具
 const spinner = ora("loading...");
+const dependencies = ['vue', 'vuex', 'vue-router'];// 定义需要按照的依赖
 
 program
     .version('0.0.1')
@@ -12,29 +14,33 @@ program
     .action(name => {
         inquirer.prompt({
             type: "input",
-            name: "age",
-            message: "input your age please",
-            default: "18"
+            name: "confirm",
+            message: "press ok to start",
+            default: "ok"
         }).then(answer => {
-            spinner.start();
-            setTimeout(() => {
-                // 修改动画样式
-
-                // Type: string
-                // Default: 'cyan'
-                // Values: 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray'
-                spinner.color = 'red';    
-                spinner.text = 'Loading rainbows';
-                setTimeout(() => {
-                    // 加载状态修改
-                    spinner.stop() // 停止
-                    spinner.succeed('Loading succeed'+" project name is " + chalk.bgRed(name) + ", author's age is " + chalk.hex('#049CDB').bold(answer.age)); // 成功 ✔
-                    // spinner.fail(text?);  失败 ✖
-                    // spinner.warn(text?);  提示 ⚠
-                    // spinner.info(text?);  信息 ℹ
-                }, 2000);
-                
-            }, 2000);
+            spinner.start(); //开始loading
+            if (answer.confirm === 'ok') {
+                // 执行安装
+                const child = crossSpawn('npm', ['install', '-D'].concat(dependencies), { 
+                    stdio: 'inherit' 
+                });
+                // 监听执行结果
+                child.on('close', function(code) {
+                    spinner.stop() // 停止loading
+                    // 执行失败
+                    if(code !== 0) {
+                        spinner.fail(chalk.bgRed('Error occurred while installing dependencies!'));  //失败 ✖
+                        process.exit(1); //退出
+                    }
+                    // 执行成功
+                    else {
+                        spinner.succeed(chalk.bgGreen('Install finished')); // 成功 ✔
+                    }
+                })
+            }else {
+                spinner.stop() // 停止loading
+                spinner.fail(chalk.bgRed('please press ok to create')); // 失败,输入不对
+            }
         })
     });
 program.parse();
